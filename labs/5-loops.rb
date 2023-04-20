cdf0746aeb9b325b9b00b74f8234654257a7bf0f
Ronika Bhatti <ronika.bhatti@kellogg.northwestern.edu>
2023-04-20 23:13:38 +0000

    challenge complete

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -29,32 +29,32 @@ index_suits = 0
 # end 
 
 
-loop do 
-    if index_ranks == ranks.size 
-        break
-    end
-    rank = ranks[index_ranks]
- loop do
-    if index_suits == suits.size
-        break
-    end
-    suit = suits[index_suits]
-    puts "#{rank} of #{suit}"
-    index_suits = index_suits + 1
-    end
-    index_ranks = index_ranks + 1
-    index_suits = 0
-end 
+# loop do 
+#     if index_ranks == ranks.size 
+#         break
+#     end
+#     rank = ranks[index_ranks]
+#  loop do
+#     if index_suits == suits.size
+#         break
+#     end
+#     suit = suits[index_suits]
+#     puts "#{rank} of #{suit}"
+#     index_suits = index_suits + 1
+#     end
+#     index_ranks = index_ranks + 1
+#     index_suits = 0
+# end 
 
 # ----------------------------
 # ----------------------------
 # SHORTCUT
-for rank in ranks
-for suit in suits   
-    # do something
-    puts "#{rank} of #{suit}"
-end
-end
+# for rank in ranks
+# for suit in suits   
+#     # do something
+#     puts "#{rank} of #{suit}"
+# end
+# end
 
 # Sample output:
 # 2 of Clubs
@@ -67,3 +67,64 @@ end
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
+# create a deck array
+deck =[]
+
+for rank in ranks
+for suit in suits   
+    # do something
+    deck.push("#{rank} of #{suit}")
+end
+end
+
+# puts deck
+
+# deal = {
+#     "card1" => deck[rand(0..51)],
+#     "card2" => deck[rand(0..51)],
+#     "card3" => deck[rand(0..51)],
+#     "card4" => deck[rand(0..51)],
+#     "card5" => deck[rand(0..51)],
+#     }
+# puts deal
+
+shuffled_deck = deck.shuffle
+# puts shuffled_deck
+
+# hand = [
+#     shuffled_deck[0],
+#     shuffled_deck[1],
+#     shuffled_deck[2],
+#     shuffled_deck[3],
+#     shuffled_deck[4]
+# ]
+
+hand = shuffled_deck[0,4]
+
+puts hand
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# FINAL CODE
+
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+deck = []
+for rank in ranks
+  for suit in suits
+    card = "#{rank} of #{suit}"
+    deck.push(card)
+  end
+end
+
+shuffled_deck = deck.shuffle
+hand = shuffled_deck[0,5]
+
+for card in hand
+  puts card
+end
