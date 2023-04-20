e1701229983520667039b31c24c4a0506d64876f
Ronika Bhatti <ronika.bhatti@kellogg.northwestern.edu>
2023-04-20 00:00:13 +0000

    notes

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,46 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+puts 2
 
 # Perform simple math with numbers
+puts 3 + 2
+puts 3 - 2
+puts 3 * 2
+puts 3 / 2.0 #adding decimal (float), knows to give precision
+puts 7 % 4 #% gives remainders
+
+puts 5 * (2 - 2) #order of operations
+
 
 # Strings
+puts "Hello, world"
 
 # Combine strings together
+puts "Hello, " + "world"
+
+puts "Tacos" * 3
 
 # Variables
+food = "Tacos"
+number = 3
+
+puts food
+puts number
+
+number = 5
+
+puts food * number
 
 # Combine strings and variables
+greeting = "heyyyy"
+
+puts "#{greeting}, world!" #in the curly brackets add variable
+
+welcome_message = "#{greeting}, world!" # underscore to separate words // don't use capital letters
+
 
 # String manipulation
+puts welcome_message.upcase
+puts welcome_message.reverse
